102151601194ce6b15a9515a530b040be58345d6
mateuswesley314@gmail.com <mateuswesley314@gmail.com>
2024-06-28 16:50:05 -0300

    padronizando urls e nomes template

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+
+
+app_name = "projects"
 urlpatterns = [
-    path('', views.projects, name='projects'),
-    path('<str:pk>', views.project, name='project')
+    path('', views.projects, name='all'),
+    path('<uuid:pk>', views.project, name='get'),
+    path('create/', views.create_project, name='create')
 ]
