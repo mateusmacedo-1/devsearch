a34fdaf26f0971c2f46eedd523c6368950960010
mateus macedo <mateuswesley314@gmail.com>
2024-07-06 11:19:25 -0300

    add link to thumb and settings for static

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
+from django.conf.urls.static import static
+from django.conf import settings
 app_name = "projects"
 urlpatterns = [
     path('', views.projects, name='all'),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('create/', views.create_project, name='create'),
     path('<uuid:pk>/edit/', views.edit_project, name='edit'),
     path('<uuid:pk>/delete/', views.delete_project, name='delete'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
