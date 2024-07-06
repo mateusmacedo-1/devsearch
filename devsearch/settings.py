ca090d21eb63716ee9778a98222c3cdc0e713cf0
mateus macedo <mateuswesley314@gmail.com>
2024-07-06 16:34:08 -0300

    fazendo integração
    entre view e urls

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects.apps.ProjectsConfig'
+    'projects.apps.ProjectsConfig',
     'users.apps.UsersConfig'
 ]
 
