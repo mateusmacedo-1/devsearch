88d3f438b6f7939c0f33b753e2bac3f44802e104
mateus macedo <mateuswesley314@gmail.com>
2024-07-06 16:15:59 -0300

    form template style
    usando widget para adicionar atributos html nos campos dinamicamente

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ('title', 'description', 'source_link', 'demo_link', 'tags')
+        fields = ('title', 'description', 'featured_image', 'source_link', 'demo_link', 'tags')
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(attrs={'class': 'form-control'})
+        }
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for k, v in self.fields.items():
+            v.widget.attrs.update({'class':'input', 'placeholder': f'Add {k}'})
