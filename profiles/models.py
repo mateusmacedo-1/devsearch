e238ff8de2663f7dce1c705b08f14ef915e2e9e3
mateus macedo <mateuswesley314@gmail.com>
2024-07-07 11:28:11 -0300

    skill model

--- profiles/models.py
+++ profiles/models.py
@@ -2,9 +2,11 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=150, blank=True, null=True)
+    location = models.CharField(max_length=150, blank=True, null=True)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     short_intro = models.CharField(max_length=100, blank=True, null=True)
@@ -20,3 +22,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+    
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
