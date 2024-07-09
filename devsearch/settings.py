6ae17df59323498ff43e762efa9b7263711a2051
mateus macedo <mateuswesley314@gmail.com>
2024-07-09 14:30:19 -0300

    add fontawasome

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects.apps.ProjectsConfig',
-    'profiles.apps.ProfilesConfig'
+    'profiles.apps.ProfilesConfig',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
--- profiles/models.py
+++ profiles/models.py
@@ -18,6 +18,7 @@ class Profile(models.Model):
     social_twitter = models.CharField(max_length=100, blank=True, null=True)
     social_youtube = models.CharField(max_length=100, blank=True, null=True)
     social_linkedin = models.CharField(max_length=100, blank=True, null=True)
+    social_stackoverflow = models.CharField(max_length=100, blank=True, null=True)
     social_website = models.CharField(max_length=100, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
